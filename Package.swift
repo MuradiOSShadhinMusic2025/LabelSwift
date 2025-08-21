49b1730d5e495165e476d5d2882488b4c0aa9b2b
MD Murad Hossain <muradhossain@shadhinmusic.com>
2025-08-21 16:21:22 +0600

    assets added

--- Package.swift
+++ Package.swift
@@ -6,16 +6,26 @@ import PackageDescription
 let package = Package(
     name: "LabelSwift",
     products: [
+        
         // Products define the executables and libraries a package produces, making them visible to other packages.
+        
         .library(
             name: "LabelSwift",
             targets: ["LabelSwift"]),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
-            name: "LabelSwift"),
+            name: "LabelSwift",
+            path: "Sources",
+            resources: [
+                .process("Resources/Media.xcassets")
+            ]
+        ),
+        
         .testTarget(
             name: "LabelSwiftTests",
             dependencies: ["LabelSwift"]
