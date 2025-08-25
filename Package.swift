40e9381677b31a29c4597a8e4eb5a743b7910eff
MD Murad Hossain <muradhossain@shadhinmusic.com>
2025-08-25 16:05:00 +0600

    update

--- Package.swift
+++ Package.swift
@@ -34,7 +34,6 @@ let package = Package(
             ],
             path: "Sources",
             resources: [
-                .process("Resources"),
                 .process("Resources/Media.xcassets"),
                 .process("Resources/Reels_header.json"),
                 .process("Resources/equalizer_animation.json"),
