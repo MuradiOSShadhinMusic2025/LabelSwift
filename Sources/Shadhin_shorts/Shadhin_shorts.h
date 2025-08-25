ea928c8d4aac87560f7c3a7c83e552e1014e51e6
MD Murad Hossain <muradhossain@shadhinmusic.com>
2025-08-25 18:08:57 +0600

    upage

---
+++ Sources/Shadhin_shorts/Shadhin_shorts.h
@@ -0,0 +1,18 @@
+//
+//  Shadhin_shorts.h
+//  Shadhin_shorts
+//
+//  Created by Maruf on 24/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shadhin_shorts.
+FOUNDATION_EXPORT double Shadhin_shortsVersionNumber;
+
+//! Project version string for Shadhin_shorts.
+FOUNDATION_EXPORT const unsigned char Shadhin_shortsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shadhin_shorts/PublicHeader.h>
+
+
